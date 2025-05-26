dd1be2958ce539190867ebbfca7dcd411c6a3931
Leif2k <leif2k@yandex.ru>
2025-05-26 15:26:48 +0300

    Add Presentation

--- app/src/main/java/com/leif2k/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/leif2k/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -1,38 +1,52 @@
 package com.leif2k.shoppinglist.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.leif2k.shoppinglist.domain.ShopItem
 import com.leif2k.shoppinglist.domain.ShopListRepository
 
 object ShopListRepositoryImpl : ShopListRepository {
 
+    private val shopListLD = MutableLiveData<List<ShopItem>>()
     private val shopList = mutableListOf<ShopItem>()
+
+    init {
+        for (i in 0 until 10) {
+            addShopItem(ShopItem("Item $i", i, true))
+        }
+    }
 
     private var autoIncrementId = 0
 
-    override fun getShopList(): List<ShopItem> {
-        return shopList.toList()
+    override fun getShopList(): LiveData<List<ShopItem>> {
+        return shopListLD
     }
 
     override fun getShopItem(shopItemId: Int): ShopItem {
-        return shopList.find { it.id == shopItemId } ?: throw RuntimeException("Element id $shopItemId wasn't found")
+        return shopList.find { it.id == shopItemId }
+            ?: throw RuntimeException("Element id $shopItemId wasn't found")
     }
 
     override fun addShopItem(shopItem: ShopItem) {
-        if (shopItem.id ==ShopItem.UNDEFINED_ID) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
             shopItem.id = autoIncrementId++
         }
         shopList.add(shopItem)
-
+        updateList()
     }
 
     override fun editShopItem(shopItem: ShopItem) {
         val item = getShopItem(shopItem.id)
         shopList.remove(item)
-        shopList.add(shopItem)
-
+        addShopItem(shopItem)
     }
 
     override fun removeShopItem(shopItem: ShopItem) {
         shopList.remove(shopItem)
+        updateList()
+    }
+
+    private fun updateList() {
+        shopListLD.value = shopList.toList()
     }
 }
